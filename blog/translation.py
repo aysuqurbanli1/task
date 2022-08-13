a6131adbff2e323b416e3149e65998fca9159274
aysuqurbanli1 <104373718+aysuqurbanli1@users.noreply.github.com>
2022-08-13 14:19:38 +0400

    Delete blog directory

--- blog/translation.py
+++
@@ -1,8 +0,0 @@
-from modeltranslation.translator import translator, TranslationOptions
-from .models import Blog
-
-class BlogTranslationOption(TranslationOptions):
-    fields = ('title', 'slug', 'description')
-
-
-translator.register(Blog, BlogTranslationOption)
